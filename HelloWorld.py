d49950a091711ac64523a9d3e94df5e35b20330d
Felype <felype.vg@gmail.com>
2024-12-03 10:48:41 +0000

    Adding Hello World and .bat file

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+
+0<0# : ^
+'''
+@echo off
+echo batch code
+python "%~f0" %*
+exit /b 0
+'''
+
+print("Hello World!")
---
+++ RunHelloWorld.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\f.fernandes\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.12\Python 3.12 (64-bit).lnk" "D:\Study\PythonHelloWorld\HelloWorld.py"
+pause
